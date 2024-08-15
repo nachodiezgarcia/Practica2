09dbbbdcdf56d675ffd2bbf919191eccc99344fd
Nacho Diez <diezgarcianacho@gmail.com>
2024-08-15 13:41:53 +0200

    nueva rama

---
+++ ficheroa.js
@@ -0,0 +1,3 @@
+console.log("*********----------");
+console.log("Fichero a")
+console.log("*********++++++++++")
--- ficherob.js
+++ ficherob.js
@@ -1,4 +1,3 @@
 console.log("-------------");
 console.log("fichero b");
 console.log("-------------");
-console.log("Había una vez un circo lleno de alegría e ilusión")
